1d0e3adc0c81da5bc02748980c07eee2f5a9c893
Saksham Bindal <2020UCP1801@mnit.ac.in>
2022-06-05 18:00:55 +0530

    final code

--- src/dbank/main.mo
+++ src/dbank/main.mo
@@ -1,5 +1,48 @@
-actor {
-  public func greet(name : Text) : async Text {
-    return "Hello, " # name # "!";
+import Debug "mo:base/Debug";
+import Time "mo:base/Time";
+import Float "mo:base/Float";
+
+actor DBank {
+  // Flexible variables
+  // var currentValue = 0;
+
+  // Orthogonal Persistence
+  // Stable variables do not loose their value on each deploy
+  stable var currentValue: Float = 0;
+  stable var startTime = Time.now();
+
+  // Private Functions
+  func showValue() {
+    Debug.print(debug_show(currentValue));
   };
-};
+
+  // Public Functions
+  // To run `dfx canister call dbank topUp '(amount)'`
+  public func topUp(amount: Float) {
+    compound();
+    currentValue += amount;
+    showValue();
+  };
+
+  public func withdraw(amount: Float) {
+    if(currentValue >= amount) {
+      compound();
+      currentValue -= amount;
+      showValue();
+    } else {
+      Debug.print("Amount too large");
+    }
+  };
+
+  // Update functions are slow so we can use query functions for read only work
+  public query func checkBalance(): async Float {
+    return currentValue;
+  };
+
+  public func compound() {
+    let currentTime = Time.now();
+    let timeElapsed = (currentTime - startTime) / 1000000000;
+    currentValue := currentValue * (1.01 ** Float.fromInt(timeElapsed));
+    startTime := Time.now();
+  }
+}
--- src/dbank_assets/src/index.js
+++ src/dbank_assets/src/index.js
@@ -1,19 +1,39 @@
 import { dbank } from "../../declarations/dbank";
 
+const refreshAmount = async () => {
+  const currentAmt = await dbank.checkBalance();
+  document.getElementById("value").innerText =
+    Math.round(currentAmt * 100) / 100;
+};
+
+const resetForm = () => {
+  document.querySelector("form").reset();
+};
+
+window.addEventListener("load", async () => {
+  await refreshAmount();
+  await dbank.compound();
+  await refreshAmount();
+});
+
 document.querySelector("form").addEventListener("submit", async (e) => {
   e.preventDefault();
-  const button = e.target.querySelector("button");
 
-  const name = document.getElementById("name").value.toString();
+  const button = e.target.querySelector("#submit-btn");
+
+  const topupAmt = document.getElementById("topup-amount").value;
+  const withdrawAmt = document.getElementById("withdraw-amount").value;
 
   button.setAttribute("disabled", true);
 
-  // Interact with foo actor, calling the greet method
-  const greeting = await dbank.greet(name);
+  if (topupAmt.length != 0) {
+    await dbank.topUp(parseFloat(topupAmt));
+  }
+  if (withdrawAmt.length != 0) {
+    await dbank.withdraw(parseFloat(withdrawAmt));
+  }
+  await refreshAmount();
 
   button.removeAttribute("disabled");
-
-  document.getElementById("greeting").innerText = greeting;
-
-  return false;
+  resetForm();
 });
